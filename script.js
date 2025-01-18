ed18135f60f27e31d65d1d06e214a53c567e4a18
SergeyFedos <feddoseev@gmail.com>
2025-01-18 14:44:35 +0000

    Added event listeners

--- script.js
+++ script.js
@@ -1,30 +1,86 @@
-const container = document.querySelector('.container');
-addRow();
+  const container = document.querySelector('.container');
+  addRow();
+  changeColor();
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  function changeColor() {
+  const allDivs = document.getElementsByClassName('column');
+  for (let i = 0; i < allDivs.length; i++) { 
+  
+
+  allDivs[i].addEventListener('mouseover', function(){
+    allDivs[i].style.backgroundColor = 'red';
+  })
+  allDivs[i].addEventListener('mouseout', function() {
+    allDivs[i].style.backgroundColor = 'white';
+  })}
+}
+
+  
 
 
 
 
 
 
-function addRow() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function addRow() {
   for(let i = 0; i < 16; i++) {
     row();
   }
-}
+  }
 
 
 
-
-function row () {
-  const rowAdd = document.createElement('div');
+  function row () {
+    const rowAdd = document.createElement('div');
     rowAdd.className = 'row';
   
-  for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 16; i++) {
     const column = document.createElement('div');
     column.className = 'column';
     rowAdd.appendChild(column);
+    }
+
+  container.appendChild(rowAdd);
+
   }
-
-container.appendChild(rowAdd);
-
-}
