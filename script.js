2405560efa368258017dda77fc83d3dd93119a15
SergeyFedos <feddoseev@gmail.com>
2025-01-19 05:44:59 +0000

    finished project

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
-  const container = document.querySelector('.container');
-  addRow();
-  changeColor();
+    const container = document.querySelector('.container');
+    addRow();
+    changeColor();
+
+
+
+    const btn = document.querySelector('button');
+    btn.addEventListener('click', function() {
+    let prom = window.prompt('Put you number between 0 - 99');
+    prom = Number(prom);
+    if (prom < 0|| prom >= 100 || isNaN(prom)) {alert('ERROR! You should put number between 0 - 99')}
+    else {addRow2(prom);}
   
+    
   
+    })
+
+
   
+    function addRow2(prom) {
+    container.innerHTML = '';
+    
+    for(let i = 0; i < prom; i++) {
+      rowAdd2(container, prom);
+    }
+    function rowAdd2 (container, prom) {
+    const rowAdd = document.createElement('div');
+    rowAdd.className = 'row';
   
-  
-  
-  
+    for (let i = 0; i < prom; i++) {
+    const column = document.createElement('div');
+    column.className = 'column';
+    rowAdd.appendChild(column);
+    }
+    changeColor();
+
+  container.appendChild(rowAdd);
+
+  }
+  }
   
   
   
@@ -18,60 +48,23 @@
   
   
   function changeColor() {
-  const allDivs = document.getElementsByClassName('column');
+  const allDivs = document.querySelectorAll('.column');
+  const randomColor = getRandomColor();
   for (let i = 0; i < allDivs.length; i++) { 
-  
-
   allDivs[i].addEventListener('mouseover', function(){
-    allDivs[i].style.backgroundColor = 'red';
+    allDivs[i].style.backgroundColor = randomColor;
   })
-  allDivs[i].addEventListener('mouseout', function() {
-    allDivs[i].style.backgroundColor = 'white';
-  })}
-}
+ 
+  }}
 
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   function addRow() {
   for(let i = 0; i < 16; i++) {
     row();
   }
   }
 
-
-
-  function row () {
+    function row () {
     const rowAdd = document.createElement('div');
     rowAdd.className = 'row';
   
@@ -81,6 +74,12 @@
     rowAdd.appendChild(column);
     }
 
-  container.appendChild(rowAdd);
+    container.appendChild(rowAdd);
 
-  }
+    }
+    function getRandomColor(){
+      const r = Math.floor(Math.random() * 256)
+      const g = Math.floor(Math.random() * 256)
+      const b = Math.floor(Math.random() * 256)
+      return `rgb(${r}, ${g}, ${b})`;
+    }
